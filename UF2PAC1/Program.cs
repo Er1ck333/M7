96965cbd67576c2edebc7a6b0acc13694e676185
Er1ck333 <153616027+Er1ck333@users.noreply.github.com>
2025-01-17 16:26:14 +0100

    Add files via upload

---
+++ UF2PAC1/Program.cs
@@ -0,0 +1,17 @@
+namespace UF2_PAC1F
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
